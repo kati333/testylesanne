861428c4a9ca8c85417521d9b25904504c0363b6
kati3 <kati099@hotmail.com>
2016-10-05 16:34:31 +0300

    Task3 final

--- js/main_map.js
+++ js/main_map.js
@@ -7,8 +7,6 @@ $(document).ready(function(){
     map.addLayer(basemap);
     var clickValue = false;
 
-
-
 	var drawnItems = new L.FeatureGroup();
 	map.addLayer(drawnItems);
 
@@ -35,13 +33,17 @@ $(document).ready(function(){
 	map.addControl(drawControl);
 
 	// user events triggered when the new feature has been created
+	var layerPopup = new L.Popup();
+	var layer;
+
 	map.on('draw:created', function (e) {
-		var type = e.layerType,
-			layer = e.layer;
+		var type = e.layerType;
+			layer = e.layer; // layer that contains drawn features
 
 		if (type === 'polygon') {
-			layer.bindPopup("Sees");
+			layer.bindPopup("Inside");
 		}
+
 
 		drawnItems.addLayer(layer);
 	});
@@ -61,11 +63,54 @@ $(document).ready(function(){
 	map.on('click', function(e) {
 		// click event will get a respond when user has stopped drawing
 		if (clickValue) {
+			var vertices_x,vertices_y,pnts_in_polygon,longitude,latitude;
+
+			vertices_x = [];
+			vertices_y = [];
+			pnts_in_polygon = layer._latlngs.length;
+			longitude = e.latlng.lng;
+			latitude = e.latlng.lat;
+
+			// populate the list of vertices_x
+			for (var i = 0; i < layer._latlngs.length; i++){
+				// console.log(layer._latlngs[i].lat);
+				vertices_y.push(layer._latlngs[i].lat);
+			}
+			// console.log(vertices_x);
+			// populate the list of vertices_y
+			for (var i = 0; i < layer._latlngs.length; i++){
+				// console.log(layer._latlngs[i].lat);
+				vertices_x.push(layer._latlngs[i].lng);
+			}
+			// console.log(vertices_y);
+			var check = is_in_polygon(pnts_in_polygon,vertices_x,vertices_y,longitude,latitude);
+			console.log(check);
+
 			popup.setLatLng(e.latlng); 
-			popup.setContent('Väljas');
-			popup.openOn(map);
-			// map.openPopup(popup);
+			if (check === true) {
+				popup.setContent('Inside');
+			} if (check === false) {
+				popup.setContent('Outside')
+			}
+			popup.openOn(map); 
+    
+
 		}
 	});
+
+
+	function is_in_polygon(pnts_in_polygon,vertices_x,vertices_y,longitude,latitude) {
+		var inside = false;
+		var i,j;
+		i=j=intersect=0;
+
+		 for (i = 0, j = pnts_in_polygon -1; i<pnts_in_polygon; j = i++ ) {
+		 	var intersect = ( (vertices_y[i] >  latitude) != (vertices_y[j] > latitude)) && (longitude < (vertices_x[j] - vertices_x[i]) * (latitude - vertices_y[i]) / (vertices_y[j] - vertices_y[i]) + vertices_x[i]);
+
+		    if (intersect){ 
+		    	inside = !inside;} 
+		 }
+		 return inside;
+	}
 
 });
